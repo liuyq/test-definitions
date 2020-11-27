4a9fc458e858f25e42f8dc599e3d5ecd3f5c7b19
Yongqin Liu <yongqin.liu@linaro.org>
2020-11-27 15:43:19 +0800

    debug-fastboot.sh: try to change the group to plugdev for fastboot devices
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/boottest/debug-fastboot.sh
+++ automated/android/boottest/debug-fastboot.sh
@@ -4,23 +4,32 @@
 id
 echo "----fastboot devices list from /sys/bus/usb/devices start----"
 fastboot_devices=""
+devpaths=""
 #ls /sys/bus/usb/devices/*/serial | while read -r device; do
 ls /sys/bus/usb/drivers/usb/*/serial | while read -r device; do
     basedir=$(dirname ${device})
     realpath_basedir=$(realpath ${basedir})
-    fastboot_devices="${fastboot_devices} ${realpath_basedir}"
     basedir_name=$(basename ${basedir})
     #interface_dir="/sys/bus/usb/devices/${basedir_name}/${basedir_name}:1.*"
     ls -1d /sys/bus/usb/devices/${basedir_name}/${basedir_name}:1.* 2>/dev/null| while read -r interface; do
         bInterfaceClass=$(cat ${interface}/bInterfaceClass)
         bInterfaceSubClass=$(cat ${interface}/bInterfaceSubClass)
         bInterfaceProtocol=$(cat ${interface}/bInterfaceProtocol)
+        devnum=$(cat ${interface}/devnum)
+        busnum=$(cat ${interface}/busnum)
         if [ "X${bInterfaceClass}" = "Xff" ] && \
             [ "X${bInterfaceSubClass}" = "X42" ] && \
             [ "X${bInterfaceProtocol}" = "X03" ]; then
             serial=$(cat ${device})
             if [ ! -f ${interface}/interface ]; then
                 echo "${serial} no-interface-fastboot ${device}"
+
+                fastboot_devices="${fastboot_devices} ${realpath_basedir}"
+
+                busnum=$(printf "%03d" ${busnum})
+                devnum=$(printf "%03d" ${devnum})
+                devpath="/dev/bus/usb/${busnum}/${devnum}"
+                devpaths="${devpaths} ${devpath}"
             else
                 echo "${serial} interface-$(cat ${interface}/interface) ${device}"
             fi
@@ -35,14 +44,33 @@ echo "----fastboot devices list from fastboot devices command end----"
 echo "----list usb devices owner and group start----"
 ls -l /dev/bus/usb/*/*
 echo "----list usb devices owner and group end----"
-fastboot_devices=$(echo ${fastboot_devices})
-if [ -n "${fastboot_devices}" ]; then
-    for device in ${fastboot_devices}; do
-        echo "regenerate uevent for ${device}"
-        echo add |tee ${device}/uevent
-        # /sys/bus/usb/drivers/usb/bind and /sys/bus/usb/drivers/usb/unbind
-    done
+
+if [ "$(id -ru)" -eq 0 ]; then
+    ## chown the group to plugdev for fastboot devices
+    devpaths=$(echo ${devpaths})
+    if [ -n "${devpaths}" ]
+        for devpath in ${devpaths}; do
+            echo "chown group for ${devpath}"
+            chown :plugdev ${devpath}
+        done
+    fi
+    echo "----list usb devices owner and group after chown group to plugdev start----"
+        ls -l /dev/bus/usb/*/*
+    echo "----list usb devices owner and group after chown group to plugdev end----"
+
+    # try to regenerate the uevent for fastboot devices
+    fastboot_devices=$(echo ${fastboot_devices})
+    if [ -n "${fastboot_devices}" ]; then
+        for device in ${fastboot_devices}; do
+            echo "regenerate uevent for ${device}"
+            echo add |tee ${device}/uevent
+            # /sys/bus/usb/drivers/usb/bind and /sys/bus/usb/drivers/usb/unbind
+        done
+    fi
+    echo "----list usb devices owner and group after regenate uevent start----"
+    ls -l /dev/bus/usb/*/*
+    echo "----list usb devices owner and group after regenate uevent end----"
+    echo "----fastboot devices list from fastboot devices command after chown group and uenent regenerate start----"
+    fastboot devices
+    echo "----fastboot devices list from fastboot devices command after chown group and uenent regenerate end----"
 fi
-echo "----list usb devices owner and group after regenate uevent start----"
-ls -l /dev/bus/usb/*/*
-echo "----list usb devices owner and group after regenate uevent end----"
