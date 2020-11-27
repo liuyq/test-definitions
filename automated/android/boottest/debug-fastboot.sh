a490eb4279eff5a04e9b6b3d557e49bf363af1b1
Yongqin Liu <yongqin.liu@linaro.org>
2020-11-27 14:01:08 +0800

    debug-fastboot.sh: try to regenerate uevent
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/boottest/debug-fastboot.sh
+++ automated/android/boottest/debug-fastboot.sh
@@ -3,9 +3,12 @@
 # check the user that run this script
 id
 echo "----fastboot devices list from /sys/bus/usb/devices start----"
+fastboot_devices=""
 #ls /sys/bus/usb/devices/*/serial | while read -r device; do
 ls /sys/bus/usb/drivers/usb/*/serial | while read -r device; do
     basedir=$(dirname ${device})
+    realpath_basedir=$(realpath ${basedir})
+    fastboot_devices="${fastboot_devices} ${realpath_basedir}"
     basedir_name=$(basename ${basedir})
     #interface_dir="/sys/bus/usb/devices/${basedir_name}/${basedir_name}:1.*"
     ls -1d /sys/bus/usb/devices/${basedir_name}/${basedir_name}:1.* 2>/dev/null| while read -r interface; do
@@ -32,3 +35,13 @@ echo "----fastboot devices list from fastboot devices command end----"
 echo "----list usb devices owner and group start----"
 ls -l /dev/bus/usb/*/*
 echo "----list usb devices owner and group end----"
+fastboot_devices=$(echo ${fastboot_devices})
+if [ -n "${fastboot_devices}" ]; then
+    for device in ${fastboot_devices}; do
+        echo add |tee ${device}/uevent
+        # /sys/bus/usb/drivers/usb/bind and /sys/bus/usb/drivers/usb/unbind
+    done
+fi
+echo "----list usb devices owner and group after regenate uevent start----"
+ls -l /dev/bus/usb/*/*
+echo "----list usb devices owner and group after regenate uevent end----"
