a20c0a61bcf0641681530a87f35dd54d3e95b983
Yongqin Liu <yongqin.liu@linaro.org>
2020-12-08 22:01:05 +0800

    monitor_fastboot.sh: add support for the docker method
    when used with the docker method, the images are downloaded
    to the /lava-downloads/ directory in the docker side
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/noninteractive-tradefed/monitor_fastboot.sh
+++ automated/android/noninteractive-tradefed/monitor_fastboot.sh
@@ -2,6 +2,12 @@
 while true;
 do
     date
-    fastboot boot /lava-lxc/*boot*.img
+    if [ -f /lava-lxc/*boot*.img ]; then
+        # for lxc container method
+        fastboot boot /lava-lxc/*boot*.img
+    else
+        # for docker method
+        fastboot boot /lava-downloads/*boot*.img
+    fi
     sleep 30
 done
