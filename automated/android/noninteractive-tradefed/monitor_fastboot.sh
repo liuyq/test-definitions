ee0e744d8d73460faa7df975c46c505abbdcf1fd
Yongqin Liu <yongqin.liu@linaro.org>
2020-12-14 00:07:58 +0800

    monitor_fastboot.sh: update to support for the docker method
    where the boot image is put under the /lava-downloads directory
    inside the docker
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/noninteractive-tradefed/monitor_fastboot.sh
+++ automated/android/noninteractive-tradefed/monitor_fastboot.sh
@@ -2,6 +2,16 @@
 while true; do
     date
     echo "Run fastboot continue to wait and reboot the device again"
-    fastboot continue
+    # fastboot continue # still not supported for all platforms, like db845c, x15
+    if [ -f /lava-lxc/*boot*.img ]; then
+        # for lxc container method
+        fastboot boot /lava-lxc/*boot*.img
+    elif [ -f /lava-downloads/*boot*.img ]; then
+        # for docker method
+        fastboot boot /lava-downloads/*boot*.img
+    else
+        echo "No boot image found under /lava-lxc and /lava-downloads, please check and try again!"
+        exit 1
+    fi
     sleep 30
 done
